7a22a979e501564f68f9e1370be4bc9ca827a864
wegahta <gehawegahta@gmail.com>
2020-08-03 22:58:20 -0500

    adding

--- W3D1/test.js
+++ W3D1/test.js
@@ -1,26 +1,26 @@
 
 
 
-describe("Account Test", function () {
-    let acc = new Account(1);
-
-
-    it("Testing getNumber of Account", function () {
+describe("Account Test", function(){
+    let acc=new Account(1);
+    
+   
+    it("Testing getNumber of Account", function(){
         assert.equal(100, new Account(100).getNumber());
     });
 
-    it("Testing deposit of Account ", function () {
+    it("Testing deposit of Account ", function(){
         acc.deposit(300);
         assert.equal(300, acc.getBalance());
-
+        
     });
 
-    it("Testing with draw and getBalance of Account", function () {
+    it("Testing with draw and getBalance of Account", function(){
         acc.withdraw(100);
         assert.equal(200, acc.getBalance());
-
+        
     });
-
+        
 });
 
 
@@ -30,28 +30,28 @@ describe("Account Test", function () {
 
 
 
-describe("Saving Account Test", function () {
+describe("Saving Account Test", function(){
 
-    let saving = new Savingsaccount(2);
+    let saving=new Savingsaccount(2);
 
-    it("Testing getInterest of SavingAccount", function () {
+    it("Testing getInterest of SavingAccount", function(){
         assert.equal(5, saving.getInterest());
     });
 
-    it("Testing setInterest of SavingAccount", function () {
+    it("Testing setInterest of SavingAccount", function(){
         saving.setInterest(7);
         assert.equal(7, saving.getInterest());
     });
 
-    it("Testing addInterest of SavingAccount", function () {
+    it("Testing addInterest of SavingAccount", function(){
         saving.deposit(250);
         saving.addInterest();
 
         assert.equal(267.5, saving.getBalance());
     });
 
-    it("Testing endOfMonth of SavingAccount", function () {
-        assert.equal("Interest added SavingsAccount:" + saving.getNumber() + " balance: " + saving.getBalance() + "interest:" + (saving.getBalance() * saving._interest / 100), saving.endOfMonth());
+    it("Testing endOfMonth of SavingAccount", function(){
+        assert.equal("Interest added SavingsAccount:"+ saving.getNumber() +" balance: "+ saving.getBalance() +"interest:"+ (saving.getBalance() * saving._interest / 100),saving.endOfMonth());
     });
 })
 
@@ -62,24 +62,24 @@ describe("Saving Account Test", function () {
 
 
 
-describe("Checking Account Test", function () {
+describe("Checking Account Test", function(){
 
-    let checking = new CheckingAccount(3);
+    let checking=new CheckingAccount(3);
 
-    it("Testing getOverdraft of CheckingAccount", function () {
-
+    it("Testing getOverdraft of CheckingAccount", function(){
+        
         assert.equal(500, checking.getOverdraft());
     });
 
-    it("Testing setOverdraft of CheckingAccount", function () {
+    it("Testing setOverdraft of CheckingAccount", function(){
         checking.setOverdraft(750);
-        checking.withdraw(500);
+        checking.withdraw(500); 
         assert.equal(250, checking.getOverdraft());
     });
 
-    it("Testing endOfMonth of CheckingAccount", function () {
-
-        assert.equal("Warning, low balance CheckingAccount: " + checking.getNumber() + "balance: " + checking.getBalance() + " overdraft limit:" + checking._overdraft, checking.endOfMonth());
+    it("Testing endOfMonth of CheckingAccount", function(){
+               
+        assert.equal("Warning, low balance CheckingAccount: "+ checking.getNumber() +"balance: "+ checking.getBalance()+" overdraft limit:"+checking._overdraft, checking.endOfMonth());
     });
 
 })
@@ -91,27 +91,27 @@ describe("Checking Account Test", function () {
 
 
 
-describe("Bank  Test", function () {
+describe("Bank  Test", function(){
+    
+    let bank=new Bank();
 
-    let bank = new Bank();
-
-    it("Testing addAccount of Bank", function () {
-
+    it("Testing addAccount of Bank", function(){
+               
         assert.equal(1, bank.addAccount());
     });
 
-    it("Testing addSavingsAccount of Bank", function () {
-
+    it("Testing addSavingsAccount of Bank", function(){
+               
         assert.equal(2, bank.addSavingsAccount(6));
     });
 
-    it("Testing addCheckingAccount of Bank", function () {
-
+    it("Testing addCheckingAccount of Bank", function(){
+               
         assert.equal(3, bank.addCheckingAccount(650));
     });
-
-    it("Testing closeAccount of Bank", function () {
-
+    
+    it("Testing closeAccount of Bank", function(){
+               
         assert.equal(2, bank.closeAccount(2));
     });
 })
