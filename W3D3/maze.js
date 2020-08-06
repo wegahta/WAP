0b6a5410a525e80177c81d252f3f78e94d91207a
wegahta <gehawegahta@gmail.com>
2020-08-05 22:06:00 -0500

    Adding

---
+++ W3D3/maze.js
@@ -0,0 +1,50 @@
+var control=0;
+var counter=0;
+var reset=0;
+$(function(){
+
+    $("#maze .boundary").mouseover(turnRed);
+    $("#end").mouseover(endPoint);
+    $("#maze").mouseenter(mazeEnter)
+   // $("#maze").mouseout(mazeOut)
+    $("#start").mouseover(resetPoint);
+  
+});
+
+// function mazeOut(){
+//     reset=0;
+// }
+
+function mazeEnter(){
+    reset++;
+    control++
+}
+
+
+
+function turnRed(evt) {
+    $("#maze .boundary").addClass('youlose');
+     
+}
+
+
+
+function endPoint(evt){
+    if(counter==0 && reset>0&&control==1){
+        $("#status").text("You won");
+     
+    }
+    
+    else{
+        $("#status").text("You lost");
+        counter ++;
+        control++
+    }
+}
+
+
+function resetPoint(evt){
+    $("#maze .boundary").css('background-color','#eeeeee');
+    location.reload();
+}
+
